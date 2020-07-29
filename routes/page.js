e83a326f16b7317c764b731ab7d0b6cc7905143f
hys <jericoh@naver.com>
2020-07-30 03:34:42 +0900

    mock & routing

--- routes/page.js
+++ routes/page.js
@@ -3,7 +3,47 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/', (req, res)=>{
-  res.render('index', {title : 'Todo list'});
+  res.render('index', {
+    title : 'Todo list',
+    user : null,
+  });
+});
+
+
+router.get('/join', (req, res)=>{
+  res.render('join',{
+    title : 'join',
+    user:null,
+  });
+});
+
+router.get('/login', (req, res)=>{
+  res.render('login',{
+    title : 'login',
+    user:null,
+  });
+});
+
+router.get('/todos', (req, res)=>{
+  res.render('todos',{
+    title : 'todos',
+    user:null,
+    todos:null,
+  });
+});
+
+router.get('/write', (req, res)=>{
+  res.render('write',{
+    title : 'write',
+    user:null,
+  });
+});
+
+router.get('/edit', (req, res)=>{
+  res.render('edit',{
+    title : 'edit todo',
+    user:null,
+  });
 });
 
 module.exports=router;
